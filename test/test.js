b2334ff4d48ae97bdc1b9cdee084165b64df8df5
徐建辉 <1594776259@qq.com>
2021-12-22 00:09:30 +0800

    前端工程化处理

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
-let a = 1
-switch(a) {
-  case 1:
-    console.log('1')
-  case 2:
-    console.log('2')
-    return
-  case 3:
-    console.log('3')
-    break
+let arr1 = []
+let subarr1 = [1, 2, 3]
+let subarr2 = [3, 4]
+let subarr3 = [11, 2, 13]
+arr1.push(subarr1,subarr2,subarr3)
+let temp = copyArr(arr1)
+temp[0].pop()
+console.log(temp)
+function copyArr(arr) {
+  let temp = []
+  arr.map(item => {
+    temp.push(item.slice(0))
+  })
+  return temp
 }
+
+console.log(arr1)
+
