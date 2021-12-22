82e0ea1de9348fa4813e892f85415cf75ffe81b9
徐建辉 <1594776259@qq.com>
2021-12-23 00:20:23 +0800

    vite-mini => app.vue

---
+++ build_utils/vite-mini/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1  @click="add">{{count}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count
+      }
+    },
+    methods: {
+      add() {
+        this.count ++
+      }
+    }
+  }
+</script>
--- build_utils/vite-mini/src/main.js
+++ build_utils/vite-mini/src/main.js
@@ -1,5 +1,11 @@
 import { Add } from './xjh.js';
 // 这样就是一个正确的打开方式,一定要注意 如果是处理就一定要彻底,这个才是正确的
-import vue from 'vue';
-Add()
+import Vue from 'vue';
+import App from './app.vue';
 console.log(123)
+const app = new Vue({
+  el: '#app',
+  components: {
+    App
+  }
+})
