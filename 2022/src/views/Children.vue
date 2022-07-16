d707bfd6693b008be93a918c9dacb2957bf6ac39
mynameszhazh <1594776259@qq.com>
2022-07-16 23:25:42 +0800

    use props validator

---
+++ 2022/src/views/Children.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>我是 home 的一个 子组件: {{name}}</div>
+</template>
+
+<script>
+export default {
+  name: "chilren",
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        // 这个值必须匹配下列字符串中的一个
+        return ["success", "text", "danger"].indexOf(value) !== -1;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- 2022/src/views/Home.vue
+++ 2022/src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>{{home}}</div>
+  <div>
+    <div>{{home}}</div>
+    <Children name="text2"></Children>
+  </div>
 </template>
 
 <script>
+import Children from './Children.vue'
 export default {
   name: 'Home',
   data() {
     return {
       home: "我就是一个home2",
     };
+  },
+  components: {
+    Children
   },
 };
 </script>
