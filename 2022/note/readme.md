32e2e4d199d926f50d0e7f4eb4da9340e2c7283f
mynameszhazh <1594776259@qq.com>
2022-07-18 20:36:09 +0800

    小心vuex vue.observable 的使用

--- 2022/note/readme.md
+++ 2022/note/readme.md
@@ -9,3 +9,30 @@
 
 - 介绍
   - 这个就像是 vuex 一样的东西，只是这个东西会小一点
+
+### 动态组件
+
+- 很好的一个方式，参数传递的考虑方式，也是存在一些偏差的 
+
+### extend
+
+- 这就是一个关键的案列
+```js
+// 创建构造器
+var Profile = Vue.extend({
+  template: '<p>{{extendData}}</br>实例传入的数据为:{{propsExtend}}</p>',//template对应的标签最外层必须只有一个标签
+  data: function () {
+    return {
+      extendData: '这是extend扩展的数据',
+    }
+  },
+  props:['propsExtend']
+})
+
+// 创建的构造器可以挂载到元素上,也可以通过 components 或 Vue.component()注册使用
+// 挂载到一个元素上。可以通过propsData传参.
+new Profile({propsData:{propsExtend:'我是实例传入的数据'}}).$mount('#app-extend')
+
+// 通过 components 或 Vue.component()注册
+Vue.component('Profile',Profile)
+```
--- 2022/src/views/Home.vue
+++ 2022/src/views/Home.vue
@@ -1,24 +1,33 @@
 <template>
   <div>
-    <div>{{ home }}-observable data{{count}}</div>
-    <Children
+    <div>{{ home }}-observable data{{ count }}</div>
+    <button @click="changeComponet">loginCom</button>
+    <button @click="changeComponet(1)">chilrenCom</button>
+    <!-- <Children
       name="text"
       :title.sync="title"
       alt="alt"
       @change="change"
-    ></Children>
+    ></Children> -->
+    <transition name="slide-fade">
+      <keep-alive>
+        <component :is="currentComponent" name="text" title="titel"></component>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
 <script>
 import Children from "./Children.vue";
-import {store} from '../store/observable'
+import Login from "./Login.vue";
+import { store } from "../store/observable";
 export default {
   name: "Home",
   provide() {
     return {
       //provide 是一个对象,提供一个属性或方法
       foo: this.fooMsg,
+
       fooMethod: () => {
         console.log("父组件 fooMethod 被调用");
       },
@@ -27,35 +36,68 @@ export default {
   data() {
     return {
       home: "我就是一个home2",
+      detailInfo: {
+
+      },
+      currentComponent: null,
       fooMsg: "这是 foo",
-      title: '我是标题'
+      title: "我是标题",
     };
   },
   computed: {
     count() {
-      return store.count 
-    }
+      return store.count;
+    },
+  },
+  mounted() {
   },
   watch: {
     title(newValue, oldValue) {
-      console.log(newValue, oldValue)
-    }
+      console.log(newValue, oldValue);
+    },
   },
   created() {
     setTimeout(() => {
       this.fooMsg = "我就是一个foo";
-      console.log('我就是一个foo')
+      console.log("我就是一个foo");
     }, 2000);
   },
   methods: {
     change() {
       console.log("我发生了一些改变！！");
     },
+    changeComponet(index) {
+      if (index == 1) {
+        this.currentComponent = Children;
+      } else {
+        this.currentComponent = Login;
+      }
+    },
   },
   components: {
     Children,
+    Login,
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
